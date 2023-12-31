79bade68edb2d91729bc4986134332f9448698c3
Pedro-Gavin <114147613+Pedro-Gavin@users.noreply.github.com>
2023-12-31 18:46:43 +0100

    Update Fila_Unica.c

--- Fila_Unica.c
+++ Fila_Unica.c
@@ -1,1 +1,20 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <time.h>
+#include <unistd.h>
+
+#define Ruta_Logs 
+#define Archivo_Logs
+
+
+
+int main(int argc, char const *argv[]){
+
+    pthread_t cajeros, reponedor, clientes;
+    //pthread_create();
+    return 0;
+}
+
