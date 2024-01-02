76556cee45e332f4994e97a58f7dd63537a57f37
Mario Fernández Garcia <mferng52@estudiantes.unileon.es>
2024-01-02 11:42:45 +0100

    Update Fila_Unica.c

--- Fila_Unica.c
+++ Fila_Unica.c
@@ -6,15 +6,21 @@
 #include <time.h>
 #include <unistd.h>
 
-#define Ruta_Logs 
-#define Archivo_Logs
+#define ClientesMax 20 
+#define Cajeros 3
 
-
+void cajeroFuncion(void arg);
+void reponedorFuncion(void arg);
+void clienteFuncion(void arg);
+//manejador señales (FALTA)
 
 int main(int argc, char const *argv[]){
+    //implmentacion manejador de señales
 
     pthread_t cajeros, reponedor, clientes;
     //pthread_create();
     return 0;
 }
-
+void cajeroFuncion(void arg);
+void reponedorFuncion(void arg);
+void clienteFuncion(void arg);
